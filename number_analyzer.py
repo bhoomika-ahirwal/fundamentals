4940b97ae848592610371df5661984fefe9b806f
bhoomika <bhoomikaahirwal.lwcs@gmail.com>
2026-08-10 19:30:20 +0530

    Add number analyzer

---
+++ number_analyzer.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+if num != 0:
+    if num % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
